53bee87e7fc14ce602be5fbd6e8a50f533fb0abb
Puneetchauhan9 <pc3622433@gmail.com>
2025-08-12 11:50:48 +0530

    Selection_Sort.cpp

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -41,6 +41,26 @@ int main(){
         cout << arr[k] << " ";
     }
 
+
+"Selection Sort ka reverse version"
+
+for(int i = 5; i>0; i--){
+  int  index = i;
+
+  for(int j = i-1; j>=0; j--){
+    if(arr[j]>arr[index]){
+        index = j;
+    }
+  }
+  swap(arr[index] , arr[i]);
+
+}
+   for(int k = 0; k<6; k++){
+    cout<<arr[k]<<" ";
+   }
+
+}
+
     return 0;
 }
 

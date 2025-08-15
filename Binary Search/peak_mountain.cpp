3c6ad26bab896faa04ec4d6592b2f6bcfc45d4a9
Puneetchauhan9 <pc3622433@gmail.com>
2025-08-15 23:14:36 +0530

    peak_mountain.cpp

--- Binary Search/peak_mountain.cpp
+++ Binary Search/peak_mountain.cpp
@@ -1,1 +1,42 @@
 ECHO is on.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7] = {2,4,6,8,10,8,5};
+    int start = 0 , end = 6 ,mid, peak;
+    // while(start<=end){
+    //      mid = start + (end-start)/2;
+    //     if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+    //         cout<<"Peak Element is : "<<arr[mid] <<" at index "<< mid <<" ";
+    //         return 0;
+    //     }
+    //     else if(arr[mid]>arr[mid-1]){
+    //         peak = mid;
+    //         start = mid+1;
+    //     }
+    //     else{
+    //         end = mid-1;
+    //     }
+    // }
+
+
+     while(start<=end){
+        mid = start + (end-start)/2;
+        if((mid == 0 || arr[mid]<arr[mid-1]) && 
+        (mid == 6 || arr[mid]<arr[mid+1])){
+             cout<<"minimum is "<<mid<<" ";
+
+        }
+        //  else if(arr[mid]<arr[mid-1] && arr[mid]<arr[mid+1]){
+        //     cout<<"minimum is "<<mid<<" ";
+         
+         else if(arr[mid]>arr[mid-1]){
+           end = mid-1;
+         }
+         else{
+             start = mid+1;
+         }
+       } 
+       return -1;
+    }

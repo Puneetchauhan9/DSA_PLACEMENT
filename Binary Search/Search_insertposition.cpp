64d1db70c8cce6c732b4b48725bc62ff231e3cde
Puneetchauhan9 <pc3622433@gmail.com>
2025-08-15 23:15:38 +0530

    Search_insertposition.cpp

--- Binary Search/Search_insertposition.cpp
+++ Binary Search/Search_insertposition.cpp
@@ -1,1 +1,24 @@
 "Added Search Insert Position"
+
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[8] = {1,4,6,8,10,14,16,18};
+    int target = 5;
+    int start = 0 , end = 7;
+    while(start<=end){
+        int mid = start + (end - start)/2;
+        if(arr[mid] == target){
+            cout<<"target is at index:"<<mid<<" ";
+            return 0;
+        }
+        else if(arr[mid]<target){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+     cout<<"Suitable index For "<<target<< " is "<< start <<" ";
+
+  }
